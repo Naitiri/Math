ab19b4bc11797219aa26656c584e78288cde3c0f
Naitiri <tanya_krugl@mail.ru>
2019-06-17 00:01:35 +0300

    1

--- src/lesson_3/task_8.java
+++ src/lesson_3/task_8.java
@@ -10,5 +10,29 @@ package lesson_3;
 public class task_8 {
     public static void main(String[] args) {
 
+        double a = 3;
+        double b = 4;
+        double c = 5;
+        double x;
+        double d;
+
+        if (a == 0) {
+
+
+        } else {
+            d = b * b + 4 * a * c;
+
+            if (d < 0) {
+
+            } else if (d == 0) {
+
+            } else {
+                x = (- b + Math.sqrt(d)) / 2 * a;
+            }
+
+        }
+
+        System.out.println(x);
+        System.out.println(a * (x * x) + b * x + c == 0);
     }
 }
