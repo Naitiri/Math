ea54e09414a01ab825f1e31c253d00c4da11868b
Naitiri <tanya_krugl@mail.ru>
2019-06-19 20:48:24 +0300

    1

---
+++ src/lesson_3/Divider.java
@@ -0,0 +1,19 @@
+/*Определить, является ли число а делителем числа b*/
+
+package lesson_3;
+
+public class Divider {
+    public static void main(String[] args) {
+
+        double a = 100;
+        double b = 1000;
+
+        if (b%a == 0) {
+
+            System.out.println("Yes");
+        } else {
+
+            System.out.println("No");
+        }
+    }
+}
